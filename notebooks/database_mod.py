ac65ccf516ebf211231996c9b34ee62dfd42dd46
devriesewouter89 <devriese.wouter@gmail.com>
2022-03-08 10:52:48 +0100

    Update database_mod.py
    bugfix for python imports

--- notebooks/database_mod.py
+++ notebooks/database_mod.py
@@ -1,12 +1,17 @@
-# usage: download the file via !curl https://raw.githubusercontent.com/luca-arts/seeingtheimperceptible/main/notebooks/database_mod.py -o /content/database_mod.py
+'''
+  usage: download the file to a notebook via 
+  !curl https://raw.githubusercontent.com/luca-arts/seeingtheimperceptible/main/notebooks/database_mod.py -o /content/database_mod.py
+'''
 def link_nextcloud(Nextcloud_URL="https://cloud.bxlab.net/remote.php/dav/files/colab/colabfiles/"):
   '''
   link_nextcloud is a function which mounts a nextcloud database ´Nextcloud_URL´ to /content/database
   returns the link to the database
   '''
+  from subprocess import run, Popen, PIPE, STDOUT
   import os
-  !apt install -y davfs2  > /dev/null
-
+  run(['apt','install','-y','davfs2'])
+  run(['pip','install','getpass'])
+  import getpass
   nextcloud = "/content/database"
   os.makedirs(nextcloud, exist_ok=True)
 
@@ -14,13 +19,15 @@ def link_nextcloud(Nextcloud_URL="https://cloud.bxlab.net/remote.php/dav/files/c
     os.remove("/etc/fstab")
   with open("/etc/fstab" , "a") as f:
     f.write(Nextcloud_URL + " " + nextcloud + " davfs user,rw,auto 0 0")
-  !mount {nextcloud}
-
-  # open existing text file
+  un = input("what's the username for nextcloud? ")
+  pw= getpass.getpass("what's the password for user {}? ".format(un))
+  p = run(['mount',nextcloud], stdout=PIPE, input='{}\n{}\n'.format(un, pw), encoding='ascii')
+  print(p.returncode)
+  print(p.stdout)
   txt = "/etc/fstab"
   viewTxt = open(txt, "r")
   txtContent = viewTxt.read()
-  print("txtContent: {}".format(txtContent))
+  print("content of /etc/fstab: {}".format(txtContent))
   
   return nextcloud
   
@@ -30,7 +37,6 @@ def create_io(database='/content/database/', topic=None, library=None):
   An output folder for experiments gets created via `topic/library` 
   '''
   import shutil, os
-  # clean and rebuild the image folders
   input_folder = database+topic
 
   output_folder = database+topic+'/'+library
